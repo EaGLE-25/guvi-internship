b1951f0d98e2579158bd19eb10d1abb57138d4c8
sai darshan <saidarshan27@gmail.com>
2021-05-22 16:16:27 +0530

    remove .trim() from username

--- dist/scripts/js/signin.js
+++ dist/scripts/js/signin.js
@@ -36,7 +36,7 @@ signinForm.submit(function(e){
         .then(data=>{
             if(data.code>=200 && data.code<=299){
                 const accessToken = data.accessToken;
-                const username = data.username.trim();
+                const username = data.username;
 
                 sessionStorage.setItem("accessToken",accessToken);
                 sessionStorage.setItem("username",username);
