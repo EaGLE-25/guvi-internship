03d0ae29bf4178c4d87c207f787b93bd8ff06823
sai darshan <saidarshan27@gmail.com>
2021-05-22 14:55:02 +0530

    debug JWT heroku

--- dist/scripts/php/serviceClasses/UserService.php
+++ dist/scripts/php/serviceClasses/UserService.php
@@ -59,7 +59,7 @@
         throw new UnauthorizedException($e->getMessage(),401);
       }
       if ($token->nbf > $now->getTimestamp() || $token->exp < $now->getTimestamp() || $token->username !== $username){
-          throw new UnauthorizedException($e->getMessage(),401);
+          throw new UnauthorizedException("Please login",401);
       }
     }
 
